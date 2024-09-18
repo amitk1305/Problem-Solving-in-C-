7b136a5f150835ae022cdba250a6010b98b6dd3a
Amit Kumar <kumaramityadaw@gmail.com>
2024-09-18 17:15:44 +0530

    Time: 6 ms (18.05%), Space: 10.1 MB (29.39%) - LeetHub

---
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        // Convert each character to its numerical value and build a string
+        string numericString = "";
+        for (char ch : s) {
+            numericString += to_string(ch - 'a' + 1);
+        }
+
+        // Apply digit sum transformations k times
+        while (k-- > 0) {
+            int digitSum = 0;
+            for (char digit : numericString) {
+                digitSum += digit - '0';
+            }
+            // If the digit sum is less than 10, we can break early
+            if (digitSum < 10) {
+                return digitSum;
+            }
+            numericString = to_string(digitSum);
+        }
+
+        // Convert the final string to integer and return
+        return stoi(numericString);
+    }
+};
