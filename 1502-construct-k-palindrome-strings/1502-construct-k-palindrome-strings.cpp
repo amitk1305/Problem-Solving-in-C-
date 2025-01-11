7a979733bd448f4cdab4c1db29f32760fb890d5f
Amit Kumar <kumaramityadaw@gmail.com>
2025-01-11 17:09:09 +0530

    Time: 4 ms (62.59%), Space: 15.1 MB (33.92%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        if (s.length() < k) return false; 
+        unordered_map<char, int> freq; 
+        for (char c : s) { freq[c]++; } 
+        int odd = 0; 
+        for (auto& pair : freq) 
+            if (pair.second % 2 != 0) 
+                odd++;
+        return odd <= k;
+    }
+};
